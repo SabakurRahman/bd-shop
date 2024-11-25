24cf20cf5d9981fdbcf5df18844f670a16185346
SabakurRahman <sabakur12360@gmail.com>
2024-11-26 00:09:14 +0600

    bd-shop page create

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { Route, Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Collection from "./pages/Collection";
+import Cart from "./pages/Cart";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Orders from "./pages/Orders";
+import PlaceOrder from "./pages/PlaceOrder";
+import Login from "./pages/Login";
+
 function App() {
-  return <h1 className="text-2xl font-bold underline">Hello world!</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/collections" element={<Collection />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/product" element={<Product />} />
+      <Route path="/product/:id" element={<Product />} />
+      <Route path="order" element={<Orders />} />
+      <Route path="palce-order" element={<PlaceOrder />} />
+      <Route path="login" element={<Login />} />
+      <Route path="*" element={<h1>Not Found</h1>} />
+    </Routes>
+  );
 }
 
 export default App;
