75dee2882e8edf4f4a316a4794747e117e26e478
SabakurRahman <sabakur12360@gmail.com>
2024-11-27 22:56:56 +0600

    hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col sm:flex-row border-gray-400">
+      <div className="w-full sm:w-1/2"></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col sm:flex-row border border-gray-400">
+      Home
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
