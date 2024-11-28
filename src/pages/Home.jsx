7b1aa04c6644698463b0f8453c9a02b1560548c9
SabakurRahman <sabakur12360@gmail.com>
2024-11-28 22:33:59 +0600

    hero section design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import Hero from "../components/Hero";
 const Home = () => {
   return (
     <div className="flex flex-col sm:flex-row border border-gray-400">
-      Home
       <Hero />
     </div>
   );
